3dd6969f68263b1b039ea682373c7e86ef90c943
V.Nearonsky <v.nearonsky@yandex.ru>
2023-02-26 20:18:46 +0300

    Решение задачи

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿
+
+string[] array = InputArray();
+string[] result = FindArray(array, 3);
+Console.WriteLine($"[{string.Join(", ", result)}]");
+
+string[] FindArray(string[] array, int n)
+{
+   int k = 0;
+   string[] resultArray = new string[FindLengthArray(array, n)];
+   for (int i = 0; i < array.Length; i++)
+   {
+      if (array[i].Length <= n)
+      {
+         resultArray[k] = array[i];
+         k++;
+      }
+   }
+   return resultArray;
+}
+
+int FindLengthArray(string[] array, int n)
+{
+   int count = 0;
+   for (int i = 0; i < array.Length; i++)
+   {
+      if (array[i].Length <= n)
+      {
+         count++;
+      }
+
+   }
+   return count;
+}
+
+string[] InputArray()
+{
+   Console.Write("Введите значения массива через пробел: ");
+   return Console.ReadLine()!.Split(" ");
+}
